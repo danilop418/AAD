632b0c71f5d2079e8244b1df5c28532b374d1a45
Dani <daniel.lopgon.4@educa.jcyl.es>
2025-09-24 17:41:07 +0200

    commit - add function readFileStream

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
+import java.io.BufferedReader
+import java.io.FileReader
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
@@ -12,7 +14,8 @@ fun main() {
 //    for (line in lines){
 //        println(line)
 //    }
-    writeFile(".\\fichero2.txt", listOf("Hola","Hola"))
+//    writeFile(".\\fichero2.txt", listOf("Hola","Hola"))
+    print(readFileStream(".\\fichero2.txt"))
 }
 
 fun createFile(filePath: String) {
@@ -36,14 +39,14 @@ fun writeFile(filePath: String, content: List<String>) {
         StandardOpenOption.APPEND)
 }
 
-
-
-
-
-
-
-
-
-
-
-
+fun readFileStream(filePath: String): List<String> {
+    val lines = mutableListOf<String>()
+    BufferedReader(FileReader(filePath)).use { reader ->
+        var line: String? = reader.readLine()
+        while (line != null) {
+            lines.add(line)
+            line = reader.readLine()
+        }
+    }
+    return lines
+}
